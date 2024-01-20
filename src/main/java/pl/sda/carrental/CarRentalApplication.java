b651bece7a56a1a09ca2e3ce81d68149e3503ec1
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-01-20 10:17:32 +0100

    Initial commit

---
+++ src/main/java/pl/sda/carrental/CarRentalApplication.java
@@ -0,0 +1,12 @@
+package pl.sda.carrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/sda/carrental/CarRentalApplicationTests.java
@@ -0,0 +1,12 @@
+package pl.sda.carrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+}
