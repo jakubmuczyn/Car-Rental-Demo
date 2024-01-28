fb46c91efdaae483ea814c4c52e0835457318824
bartoszgajewski <baroxim@gmail.com>
2024-01-28 19:19:46 +0100

    feat: Rename Role table to fit naming schema

--- src/main/java/pl/sda/carrental/model/entity/Role.java
+++ src/main/java/pl/sda/carrental/model/entity/Role.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
@@ -12,6 +13,7 @@ import lombok.*;
 @AllArgsConstructor
 @Builder
 @Entity
+@Table(name = "Roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
