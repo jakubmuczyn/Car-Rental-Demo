4b62dd26a2c58edfeed46d2741c7bbf881233ad0
bartoszgajewski <baroxim@gmail.com>
2024-01-28 11:56:27 +0100

    feat: Add display of username in top right corner of the screen after login

--- src/main/java/pl/sda/carrental/controller/HomeController.java
+++ src/main/java/pl/sda/carrental/controller/HomeController.java
@@ -1,12 +1,29 @@
 package pl.sda.carrental.controller;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.carrental.model.entity.User;
+import pl.sda.carrental.model.repository.UserRepository;
+import pl.sda.carrental.service.UserService;
 
 @Controller
 public class HomeController {
+    private final UserService userService;
+
+    public HomeController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/")
-    public String greeting() {
+    public String greeting(Model model) {
+        User user = userService
+            .getAuthenticatedUser()
+            .orElseThrow(() -> new UsernameNotFoundException("Logged in user not found."));
+        model.addAttribute("currentUser", user);
         return "home";
     }
     @GetMapping("/login")
--- src/main/java/pl/sda/carrental/service/UserService.java
+++ src/main/java/pl/sda/carrental/service/UserService.java
@@ -3,8 +3,11 @@ package pl.sda.carrental.service;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 import pl.sda.carrental.model.entity.User;
 import pl.sda.carrental.model.repository.UserRepository;
 
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,7 +25,7 @@ import java.util.stream.Collectors;
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
-    
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = findByUsernameOrEmail(username);
@@ -30,7 +34,15 @@ public class UserService implements UserDetailsService {
                 .collect(Collectors.toSet());
         return new org.springframework.security.core.userdetails.User(username, user.getPassword(), authorities);
     }
-    
+
+    public Optional<User> getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken)
+            return Optional.empty();
+
+        return Optional.of(findByUsernameOrEmail(authentication.getName()));
+    }
+
     public User findByUsernameOrEmail(String username) {
         return userRepository.findByUsernameOrEmail(username, username).orElseThrow(() ->
                 new IllegalArgumentException("Username does not exist")
