0ef0c724eecf5439df328b031eaa4c71a69e9bb3
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-02 21:45:47 +0100

    create needed repositories for entities

--- src/main/java/pl/sda/carrental/model/repository/CarRepository.java
+++ src/main/java/pl/sda/carrental/model/repository/CarRepository.java
@@ -2,8 +2,8 @@ package pl.sda.carrental.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.sda.carrental.model.entity.Reservation;
+import pl.sda.carrental.model.entity.Car;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+public interface CarRepository extends JpaRepository<Car, Integer> {
 }
