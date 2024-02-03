42d7b6c1e0129b8a9e1c54e43be1a1aa916c941c
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-03 10:53:33 +0100

    feat: WebConfig.class

--- src/main/java/pl/sda/carrental/security/WebConfig.java
+++ src/main/java/pl/sda/carrental/security/WebConfig.java
@@ -6,13 +6,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
+    public void addViewControllers(final ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/manager").setViewName("manager");
         registry.addViewController("/employee").setViewName("employee");
     }
 }
-
-
-
