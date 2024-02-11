7570e9309bf28eb3093a1bcd874d4883d9b8d216
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-11 09:37:07 +0100

    add CarController logic

--- src/main/java/pl/sda/carrental/controller/CarController.java
+++ src/main/java/pl/sda/carrental/controller/CarController.java
@@ -2,11 +2,31 @@ package pl.sda.carrental.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import pl.sda.carrental.model.dataTransfer.CarDTO;
 import pl.sda.carrental.service.CarService;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Controller
 public class CarController {
     
     private final CarService carService;
+    
+    @GetMapping("/fleet/list")
+    public String getListOfCars(Model model) {
+        List<CarDTO> carsDto = carService.getList();
+        model.addAttribute("cars", carsDto);
+        return "fleet/fleetPanelCarsList";
+    }
+    
+    @GetMapping("/fleet/{id}")
+    public String getCarById(@PathVariable Long id, Model model) {
+        CarDTO carDto = carService.getById(id);
+        model.addAttribute("car", carDto);
+        return "fleet/fleetPanelCar";
+    }
 }
