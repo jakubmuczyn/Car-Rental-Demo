73f3ce24152f2cb4bbb09014a39a27cbfdbca0c0
bartoszgajewski <baroxim@gmail.com>
2024-02-11 12:55:51 +0100

    Remove option to deactivate managers

--- src/main/java/pl/sda/carrental/model/dataTransfer/mappers/UserMapper.java
+++ src/main/java/pl/sda/carrental/model/dataTransfer/mappers/UserMapper.java
@@ -2,20 +2,30 @@ package pl.sda.carrental.model.dataTransfer.mappers;
 
 import org.springframework.stereotype.Service;
 import pl.sda.carrental.model.dataTransfer.UserDisplayDTO;
+import pl.sda.carrental.model.entity.userEntities.Employee;
 import pl.sda.carrental.model.entity.userEntities.User;
+import pl.sda.carrental.model.repository.userRepositories.EmployeeRepository;
+import pl.sda.carrental.service.DivisionService;
 import pl.sda.carrental.service.UserService;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class UserMapper {
     private final UserService userService;
-    
-    public UserMapper(UserService userService) {
+    private final EmployeeRepository employeeRepository;
+
+    public UserMapper(UserService userService, EmployeeRepository employeeRepository) {
         this.userService = userService;
+        this.employeeRepository = employeeRepository;
     }
     
     public <T extends User> UserDisplayDTO getUserDisplayDto(T user) {
+        List<Employee> activeNonManagers = employeeRepository.findAllActiveNonManagers();
+        boolean isEmployee = employeeRepository.existsById(user.getId());
+        boolean isManager = isEmployee ? employeeRepository.findAllActiveNonManagers().stream().noneMatch(e -> Objects.equals(e.getId(), user.getId())) : false;
+
         return UserDisplayDTO.builder()
                 .id(user.getId())
                 .username(user.getUsername())
@@ -23,6 +33,7 @@ public class UserMapper {
                 .email(user.getEmail())
                 .principalRole(user.getRole().getRoleName())
                 .isActive(user.isActive())
+                .isManager(isManager)
                 .build();
     }
     
