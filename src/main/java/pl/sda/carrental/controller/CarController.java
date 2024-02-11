b1f865ef4963cbe6e08ae92e8cfdc93d669a02d5
Jakub Muczyń <jakub.muczyn@gmail.com>
2024-02-11 11:51:00 +0100

    stash commit

--- src/main/java/pl/sda/carrental/controller/CarController.java
+++ src/main/java/pl/sda/carrental/controller/CarController.java
@@ -6,6 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pl.sda.carrental.model.dataTransfer.CarDTO;
+import pl.sda.carrental.model.dataTransfer.mappers.CarMapper;
+import pl.sda.carrental.model.entity.Car;
+import pl.sda.carrental.model.repository.CarRepository;
 import pl.sda.carrental.service.CarService;
 
 import java.util.List;
@@ -15,12 +18,23 @@ import java.util.List;
 public class CarController {
     
     private final CarService carService;
+    // private final CarRepository carRepository;
+    // private final CarMapper carMapper;
+    // private final CarDTO carDTO;
     
-    @GetMapping("/fleet/list")
+    // @GetMapping("/fleet")
+    // public String getCars(Model model) {
+    //     List<Car> cars = carRepository.findAll();
+    //     List<CarDTO> carsDTO = carMapper
+    //     model.addAttribute("cars", carsDTO);
+    //     return "fleet/fleetPanel";
+    // }
+    
+    @GetMapping("/fleet")
     public String getListOfCars(Model model) {
         List<CarDTO> carsDto = carService.getList();
         model.addAttribute("cars", carsDto);
-        return "fleet/fleetPanelCarsList";
+        return "fleet/fleetPanel";
     }
     
     @GetMapping("/fleet/{id}")
--- src/main/java/pl/sda/carrental/model/dataTransfer/CarDTO.java
+++ src/main/java/pl/sda/carrental/model/dataTransfer/CarDTO.java
@@ -1,13 +1,15 @@
 package pl.sda.carrental.model.dataTransfer;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import pl.sda.carrental.model.entity.Car;
 import pl.sda.carrental.model.entity.Division;
 import pl.sda.carrental.model.entity.Reservation;
 
 import java.math.BigDecimal;
 import java.time.Year;
+
 @Data
 public class CarDTO {
 
